10c9b47cca1a2e9d335063a811dc94a347851678
gainaa <kajka.c@gmail.com>
2019-02-24 11:31:33 +0100

    console.log the API call

--- src/App.js
+++ src/App.js
@@ -8,17 +8,20 @@ const API_KEY = 'ed6b65702865a551673ff4e404b087bd';
 class App extends React.Component {
     // tworzenie funkcji, która będzie korzystała z API
     // wykorzystanie async/await
-    getWeather = async () => {
-        const api_call = await fetch(`http://api.openweathermap.org/data/2.5/forecast?q=Bardo,PL&       appid=${API_KEY}&units=metric`);
+    getWeather = async (e) => {
+        // pozbycie się przeładowania strony
+        e.preventDefault();
+        const api_call = await fetch(`http://api.openweathermap.org/data/2.5/forecast?q=Bardo,PL&appid=${API_KEY}&units=metric`);
         // każda odpowiedź, którą otrzymamy od API ma być konwertowana do json
         const data = await api_call.json();
 
+        console.log(data);
     }
     render() {
         return (
             <div>
                 <Title />
-                <Form />
+                <Form getWeather={this.getWeather} />
                 <Weather />
             </div>
         )
