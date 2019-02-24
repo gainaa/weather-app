f50676610679d918c8386a8022146bb35e2ef4de
gainaa <kajka.c@gmail.com>
2019-02-24 11:50:20 +0100

    declared initial state & setState

--- src/App.js
+++ src/App.js
@@ -6,6 +6,13 @@ import Weather from './components/Weather/Weather';
 const API_KEY = 'ed6b65702865a551673ff4e404b087bd';
 
 class App extends React.Component {
+    state = {
+        temperature: undefined,
+        city: undefined,
+        humidity: undefined,
+        description: undefined,
+        error: undefined,
+    }
     // tworzenie funkcji, która będzie korzystała z API
     // wykorzystanie async/await
     getWeather = async (e) => {
@@ -16,7 +23,13 @@ class App extends React.Component {
         const api_call = await fetch(`http://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${API_KEY}&units=metric`);
         // każda odpowiedź, którą otrzymamy od API ma być konwertowana do json
         const data = await api_call.json();
-
+        this.setState({
+            temperature: data.main.temp,
+            city: data.name,
+            humidity: data.main.humidity,
+            description: data.weather[0].description,
+            error: '',
+        });
         console.log(data);
     }
     render() {
